1ffffad4070f951df36710c166ee8c9105896a27
André Miranda <andre.miranda@fiocruz.br>
2023-11-14 11:10:59 -0300

    adicionando rotas no arquivo App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 
 import './App.css';
+import Header from './components/header';
+
+import { Route, Routes } from 'react-router-dom';
+
+function Solicitacao(){
+  return(
+    <h1>Página de solicitação</h1>
+  )
+}
+
+function ListaSolicitao(){
+  return(
+    <h1>Página de listagem</h1>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      
+        <Header />
+        <Routes>
+          <Route path='/' element={ <Solicitacao /> }/>
+          <Route path='listar' element={ <ListaSolicitao /> }/>
+        </Routes>
     </div>
   );
 }
