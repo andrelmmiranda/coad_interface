6d0556b84fcaf0a239769660276d9e4ce8b59c8c
André Miranda <andre.miranda@fiocruz.br>
2023-11-15 19:52:16 -0300

    adicionando importe do componente Lista

--- src/components/listaSolicitacao/index.jsx
+++ src/components/listaSolicitacao/index.jsx
@@ -2,6 +2,8 @@ import { useContext } from "react";
 import { Container } from "react-bootstrap";
 import { SolicitacaoContext } from '../../context/solicitacaoContext';
 
+import { Lista } from '../lista';
+
 export function ListaSolicitao(){
     const { solicitacoes } = useContext(SolicitacaoContext);
     return(
