ac3cde3745daded46b085f1c8199dcd76280b9d9
André Miranda <andre.miranda@fiocruz.br>
2023-11-16 20:12:09 -0300

    adicionando card para vizualizacao das informacoes da solicitacao

--- src/components/lista/index.jsx
+++ src/components/lista/index.jsx
@@ -1,4 +1,7 @@
 import Table from 'react-bootstrap/Table';
+import Card from 'react-bootstrap/Card';
+import Badge from 'react-bootstrap/Badge';
+import ListGroup from 'react-bootstrap/ListGroup'
 
 export function Lista({ solicitacoes }){
 
@@ -15,25 +18,31 @@ export function Lista({ solicitacoes }){
     }
 
     return(
-        <Table striped bordered hover>
-            <thead>
-                <tr>
-                    <th>Titulo</th>
-                    <th>Requisitante</th>
-                    <th>Prioridade</th>
-                </tr>
-            </thead>
-            <tbody>
-                {
-                    solicitacoes.map(solicitacao => 
-                        <tr>
-                            <td>{ solicitacao.titulo }</td>
-                            <td>{ solicitacao.nome }</td>
-                            <td>{ prioridade(solicitacao.date) }</td>
-                        </tr>
-                    )
-                }
-            </tbody>
-        </Table>
+        <ul style={{ listStyle: 'none' }}>
+            {
+                solicitacoes.map(solicitacao => 
+                    <li>
+                        <Card style={{ width: '100%' }}>
+                            <div style={{ display: 'flex', justifyContent: 'right', margin: '10px 10px 0 0'}}>
+                                <h5><Badge bg="secondary">Prioridade: { prioridade(solicitacao.date) }</Badge></h5>
+                            </div>
+                            <Card.Body>
+                                <Card.Title>Título: { solicitacao.titulo }</Card.Title>
+                                <Card.Subtitle className="mb-2 text-muted">Tipo: { solicitacao.tipo }</Card.Subtitle>
+                                <Card.Text>
+                                    { solicitacao.descricao }
+                                </Card.Text>
+                                <ListGroup className="list-group-flush">
+                                    <ListGroup.Item>Nome: { solicitacao.nome }</ListGroup.Item>
+                                    <ListGroup.Item>Email: { solicitacao.email }</ListGroup.Item>
+                                    <ListGroup.Item>Telefone: { solicitacao.telefone }</ListGroup.Item>
+                                    <ListGroup.Item>Celular: { solicitacao.celular }</ListGroup.Item>
+                                </ListGroup>
+                            </Card.Body>
+                        </Card>
+                    </li>
+                )
+            }
+        </ul>
     );
 }
--- src/components/listaSolicitacao/index.jsx
+++ src/components/listaSolicitacao/index.jsx
@@ -3,12 +3,14 @@ import { Container } from "react-bootstrap";
 import { SolicitacaoContext } from '../../context/solicitacaoContext';
 
 import { Lista } from '../lista';
+import { Paginacao } from "../paginacao";
 
 export function ListaSolicitao(){
     const { solicitacoes } = useContext(SolicitacaoContext);
     return(
       <Container>
         <Lista solicitacoes={ solicitacoes } />
+        <Paginacao />
       </Container>
     )
 }
