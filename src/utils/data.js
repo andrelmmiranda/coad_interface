2ac9149b37b4b78c43742fb24716c1e6a1bc82ee
André Miranda <andre.miranda@fiocruz.br>
2023-11-15 21:43:44 -0300

    atualizando funcao data.js

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,7 +1,7 @@
 export function data(){
     const date = new Date();
     const dia = date.getDate();
-    const mes = date.getMonth();
+    const mes = date.getMonth() + 1;
     const ano = date.getFullYear();
 
     return `${ano}-${mes}-${dia}`;
